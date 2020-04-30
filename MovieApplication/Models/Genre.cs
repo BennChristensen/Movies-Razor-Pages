3be50b8dd68182bbc3a80374c109ff68197f4365
BennChristensen <bennzilla@gmail.com>
2020-04-30 18:57:56 +0200

    added many-many realtion between movie and genre

--- MovieApplication/Models/Genre.cs
+++ MovieApplication/Models/Genre.cs
@@ -1,8 +1,11 @@
-﻿namespace MovieApplication.Models
+﻿using System.Collections.Generic;
+
+namespace MovieApplication.Models
 {
     public class Genre
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Movie> MoviesWithThisGenre { get; set; }
     }
 }
